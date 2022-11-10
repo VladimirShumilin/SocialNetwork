9088a7cd3e1f1db2c9bed953da3dbb3721e80a74
Владимир Шумилин <vladimir.shumilin@infots.ru>
2022-11-10 17:08:57 +0300

    Добавьте файлы проекта.

---
+++ SocialNetwork.Tests/UserServiceIntegrationTests.cs
@@ -0,0 +1,49 @@
+﻿using Moq;
+using SocialNetwork.BLL.Exceptions;
+using SocialNetwork.BLL.Models;
+using SocialNetwork.BLL.Services;
+using SocialNetwork.DAL.Entities;
+using SocialNetwork.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Tests
+{
+    internal class UserServiceIntegrationTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+
+        [Test]
+        public void AddFriendMustThrowUserNotFoundException()
+        {
+  
+            var userService = new UserService();
+            Assert.Throws<UserNotFoundException>(() => userService.AddFriend("test@test.com", 10));
+        }
+
+        [Test]
+        public void AddFriendMustSuccsses()
+        {
+            var mock = new Mock<IUserRepository>();
+            mock.Setup(p => p.FindByEmail("test@test.com")).Returns(new UserEntity() { id=10 });
+
+            var userService = new UserService(mock.Object);
+
+            var userAuthenticationData = new UserAuthenticationData()
+            {
+                Email = "t@test.com"
+            };
+
+            Assert.DoesNotThrow(()=>userService.AddFriend("test@test.com",10));
+        }
+
+    }
+}
---
+++ SocialNetwork/PLL/Views/AddFriendView.cs
@@ -0,0 +1,48 @@
+﻿using SocialNetwork.BLL.Exceptions;
+using SocialNetwork.BLL.Models;
+using SocialNetwork.BLL.Services;
+using SocialNetwork.PLL.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.PLL.Views
+{
+    internal class AddFriendView
+    {
+        UserService userService;
+        public AddFriendView(UserService userService)
+        {
+            this.userService = userService;
+        }
+        public void Show(User user)
+        {
+            string? email = "";
+            try
+            {
+                
+
+                Console.WriteLine("Введите email добавляемого в друзья пользователя ");
+
+                email =  Console.ReadLine();
+           
+                this.userService.AddFriend(email, user.Id);
+
+                SuccessMessage.Show("Пользователь пользователя в друзья!");
+            }
+
+            catch (UserNotFoundException)
+            {
+                AlertMessage.Show($"Пользователя с  email:{email} не существует!");
+            }
+
+            catch (Exception ex)
+            {
+                AlertMessage.Show(ex.ToString());
+            }
+
+        }
+    }
+}
